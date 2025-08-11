dc2dc14943c496785ae23cce780486e5db689ef6
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2025-08-11 21:54:04 +0300

    users model

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class HealthEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(default = timezone.now)
+    symptoms = models.TextField (blank=True)
+    medications = models.TextField(blank=True)
+    mood = models.CharField(max_length=50, blank=True)
+    notes = models.TextField(blank=True)
+    attachment = models.FileField(upload_to='health_attachments/', null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['-date']
+        
